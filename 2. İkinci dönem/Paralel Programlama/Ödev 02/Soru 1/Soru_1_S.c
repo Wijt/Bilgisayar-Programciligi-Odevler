150f76f21f1597827f3fcc8fe13b716c7618ccd3
Furkan Kaya <8fkaya@gmail.com>
2020-05-21 07:15:11 +0300

    Paralel Programlama Ödev 2 Kaynak kodlar eklendi.
    Soru 1: Matrix scale
    Soru 2: Matrix multiplication
    _S means serial,
    _P means parallel.
    OpenMP used for parallelization.

---
+++ 2. İkinci dönem/Paralel Programlama/Ödev 02/Soru 1/Soru_1_S.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+
+#define N_ROW 60
+#define N_COL 80
+#define N_SCALE 2
+
+
+int a[N_ROW][N_COL];
+int b[N_ROW*N_SCALE][N_COL*N_SCALE];
+
+
+void MatrixPrint(int n_row, int n_col, int matrix[n_row][n_col]){
+    for (int row = 0; row < n_row; row++)
+    {
+        for (int col = 0; col < n_col; col++)
+        {
+            printf("%d\t", matrix[row][col]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main(){
+    struct timeval t;
+    double vStart, vEnd;
+
+    int counter = 0;
+
+    for (int row = 0; row < N_ROW; row++)
+    {
+        for (int col = 0; col < N_COL; col++)
+        {
+            a[row][col] = counter++;
+        }
+    }
+    
+    gettimeofday(&t, NULL);
+    vStart = t.tv_sec + t.tv_usec * 1.e-6;
+
+    for (int row = 0; row < N_ROW*N_SCALE; row+=N_SCALE)
+    {
+        for (int col = 0; col < N_COL*N_SCALE; col+=N_SCALE)
+        {
+            int data = a[row/N_SCALE][col/N_SCALE];
+            b[row][col] = data;         //TOP-LEFT CORNER
+            b[row+1][col+1] = data;     //BOTTOM-RIGHT CORNER
+            b[row][col+1] = data;       //TOP-RIGHT CORNER
+            b[row+1][col] = data;       //BOTTOM-LEFT CORNER
+        }
+    }
+
+    gettimeofday(&t, NULL);
+    vEnd = t.tv_sec + t.tv_usec * 1.e-6;
+        
+    //MatrixPrint(N_ROW, N_COL, a);
+    //MatrixPrint(N_ROW*N_SCALE, N_COL*N_SCALE, b);
+
+    printf("Scale factor - Elapsed time:\t%d\t-\t%10.12lf\n", N_SCALE, vEnd - vStart);
+
+    return 0;
+}
---
+++ 2. İkinci dönem/Paralel Programlama/Ödev 02/Soru 2/Soru_2_P.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include <omp.h>
+
+#define N 100
+
+int a[N][N];
+int b[N][N];
+int c[N][N];
+
+void MatrixPrint(int n_row, int n_col, int matrix[n_row][n_col]){
+    for (int row = 0; row < n_row; row++)
+    {
+        for (int col = 0; col < n_col; col++)
+        {
+            printf("%d\t", matrix[row][col]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main(){
+    struct timeval t;
+    double vStart, vEnd;
+
+    int counter = 1;
+
+    for (int row = 0; row < N; row++)
+    {
+        for (int col = 0; col < N; col++)
+        {
+            a[row][col] = counter++;
+            b[row][col] = 2;
+        }
+    }
+
+    gettimeofday(&t, NULL);
+    vStart = t.tv_sec + t.tv_usec * 1.e-6;
+
+    #pragma omp parallel for
+    for (int row = 0; row < N; row++)
+    {
+        for (int col = 0; col < N; col++)
+        {
+            int sum = 0;
+            for (int walker = 0; walker < N; walker++)
+            {
+                sum += a[row][walker] * b[walker][col];
+            }
+            c[row][col] = sum;
+        }
+    }
+
+    gettimeofday(&t, NULL);
+    vEnd = t.tv_sec + t.tv_usec * 1.e-6;
+      
+    //MatrixPrint(N, N, a);
+    //MatrixPrint(N, N, b);
+    //MatrixPrint(N, N, c);
+
+    printf("Elapsed time for matrix multiplication:\t%10.12lf\n", vEnd - vStart);
+    return 0;
+}
